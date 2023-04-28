b48fd99b7ebffd55ff411d8c063793bda85ef831
Pietromuresu <muresupietro66@gmail.com>
2023-04-28 18:49:55 +0200

    exercise ended

--- src/components/_Main.vue
+++ src/components/_Main.vue
@@ -1,23 +1,107 @@
 <script>
+import Card from './partials/_Card.vue';
+import products from '../../dc-comics.json';
+
 export default {
   name: 'Main',
+  components:{
+    Card
+  },
+  data(){
+    return{
+      products
+    }
+  }
 
 }
 </script>
 
 <template>
   <div>
-    <h1>Da fare domani =></h1>
+    <div class="jumbotron"></div>
+
+    <div class="products">
+      <div class="container">
+        <h3 class="currentSeriesTitle">
+          CURRENT SERIES
+        </h3>
+
+        <div class="card-wrapper">
+          <div class="cardContainer" v-for="(product, index) in products" :key="index">
+            <Card :item ="product" />
+          </div>
+        </div>
+      </div>
+      <div class="loadBtn">
+        <h4>LOAD MORE</h4>
+      </div>
+    </div>
+
+    
   </div>
 </template>
 
 <style lang="scss" scoped>
   @import '../scss/partials/variables';
+  @import '../scss/partials/general';
 
   div{
-    height: 200px;
-    background-color: black;
-    padding: 20px;
-    @include flexMe(vertical)
+    .jumbotron{
+      background-image: url(../assets/img/jumbotron.jpg);
+      background-size: cover;
+      height: 350px;
+      padding: 20px;
+      @include flexMe(vertical)
+    }
+
+
+    .products{
+
+      background: $black;
+      width: 100%;
+
+
+      .currentSeriesTitle{
+        position: absolute;
+        display: block;
+        line-height: 30px;
+        background: $blue;
+        color: white;
+        padding: 5px 20px;
+       top: calc(-40px / 2);
+      }
+    }   
+
+    .card-wrapper{
+      display: flex;
+      flex-wrap: wrap;
+      flex-basis: calc(100% / 6);
+      padding-top: 40px;
+
+      .cardContainer{
+        width: calc(100% / 6);
+
+      }
+
+
+    }
+
+    .loadBtn{
+      @include flexMe(horizontal);
+
+      h4{
+        line-height: 30px;
+        padding: 5px 20px;
+        background-color: $blue;
+        margin-bottom: 20px;
+        color: white;
+      }
+    }
+  }  
+
+  .container{
+    width: 80%;
+    margin: 0 auto; 
+    position: relative;
   }
 </style>
---
+++ src/components/partials/_Card.vue
@@ -0,0 +1,51 @@
+
+
+<script>
+
+export default {
+  name: "_Card",
+  props:{
+    item : Object
+  }
+}
+</script>
+
+<template>
+
+  <div class="card">
+    <div class="containerImg">
+      <img :src="item.thumb" alt="">
+    </div>
+    
+    <div class="name">
+      <h5>{{ item.series }}</h5>
+    </div>
+  </div>
+   
+</template>
+
+<style lang="scss" scoped>
+      .card{
+        height: 220px;
+        margin-bottom: 20px;
+        padding: 10px;
+
+
+        .containerImg{
+          height: 180px;
+          overflow: hidden;
+          margin-bottom: 10px;
+        }
+
+        img{
+          object-fit: contain;
+          width: 100%;
+          
+        }
+
+        h5{
+          color: white;
+          text-transform: uppercase;
+        }
+      }
+</style>
